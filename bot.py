b6b2ac42e714e055f037d4ff639042d951812dd0
jashan516 <jashanbeniwal2005@gmail.com>
2025-01-05 19:38:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,4 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+
 
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
@@ -22,7 +20,7 @@ class Bot(Client):
     async def start(self):
             
         await super().start()
-        print('Bot Started Powered By @VJ_Botz')
+        print('Bot Started Powered By @Jass_beniwal')
 
     async def stop(self, *args):
 
