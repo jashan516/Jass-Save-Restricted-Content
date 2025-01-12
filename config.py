77d6f51f3a0a58e125eea6ddd384e2183e84eb08
jashan516 <jashanbeniwal2005@gmail.com>
2025-01-12 20:38:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7776870492:AAHglTpmX4OCbEPS8CpIpyYGhj3OmD6Wv84")
 
 # Your API ID from my.telegram.org
 API_ID = int(os.environ.get("API_ID", "20988890"))
